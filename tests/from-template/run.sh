8093b6e6fcc1f01f3d5687c3c8c5b8ec3404c987
Dino Korah <codemedic@users.noreply.github.com>
2017-05-03 13:27:44 +0100

    Update run.sh

--- tests/from-template/run.sh
+++ tests/from-template/run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-../../redstart --db-file x.db3 --base-db-file template.db3 --migrations-dir . migrate-to 1
-../../redstart --db-file x.db3 --base-db-file template.db3 --migrations-dir . migrate-to 0
+_redstart() {
+  ../../redstart --db-file x.db3 --base-db-file template.db3 --migrations-dir .
+}
+
+_redstart migrate-to 1
+_redstart migrate-to 0
