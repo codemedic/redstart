b5889ee748fbd233427d2cf390c2f4db858adb03
Dino Korah <codemedic@users.noreply.github.com>
2017-09-23 19:56:25 +0100

    Typo fix

--- tests/from-template/run.sh
+++ tests/from-template/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 _redstart() {
-  ../../redstart --db-file x.db3 --base-db-file template.db3 --migrations-dir .
+  ../../redstart --db-file x.db3 --base-db-file template.db3 --migrations-dir . "$@"
 }
 
 _redstart migrate-to 1
